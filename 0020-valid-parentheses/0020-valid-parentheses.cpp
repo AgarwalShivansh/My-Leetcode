2231b5d21c777f0750ae2fd55047d5c3bdf984b4
Shivansh Agarwal <shivanshagarwal601@gmail.com>
2026-06-23 19:45:49 +0530

    Time: 0 ms (100%), Space: 9 MB (37.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char>st;
+        int n = s.size();
+
+        int i =0;
+        while(i<n)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[' ){
+                st.push(s[i]);
+                i++;
+            }
+            else{
+
+                if(st.size() == 0) return false ;
+ 
+                char c = st.top();
+                if((c == '(' && s[i] == ')') || 
+                   (c == '{' && s[i] == '}') || 
+                   (c == '[' && s[i] == ']'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+        }
+        if(st.size() == 0)
+        return true;
+        else
+        return false;
     }
 };
