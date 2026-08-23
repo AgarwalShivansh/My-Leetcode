e4caf0cf46ed2c3a88805908cf54c782d07e25fa
Shivansh Agarwal <shivanshagarwal601@gmail.com>
2026-08-23 11:50:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+
+    double powRec(double x, int n)
+    {
+        if(n == 0)
+            return 1;
+
+        if(n == 1)
+            return x;
+
+       
+
+        if(n % 2 != 0)
+        {
+            return x * powRec(x*x, n/2);
+        }
+
+        return powRec(x*x, n/2);
+    }
+    
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n < 0)
+        {
+            return 1/powRec(x, n);
+        }
+        return powRec(x,n);
     }
 };
